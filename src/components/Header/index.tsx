6ad3355b6216f342d1d59fc75f6129f7d86a4ace
Marcos Adriano <marcosaps@ufba.br>
2022-04-09 20:43:36 -0300

    feature: add AdminTemplate

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import Logo from '../../assets/assistente.png';
+
+const Header = () => {
+  return (
+    <div>
+      <header className="d-flex  align-items-center justify-content-start w-100 app-header">
+        <img src={Logo} className="app-logo" alt="logo" />
+        <h1 className="app-title">ICIA</h1>
+      </header>
+    </div>
+  );
+};
+
+export default Header;
