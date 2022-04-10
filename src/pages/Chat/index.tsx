332b9a313d18492b5f448df2b8ccfbba619bb7b6
Mateus Machado <mbmachado@outlook.com>
2022-04-10 20:06:12 -0300

    complestes chat structure

--- src/pages/Chat/index.tsx
+++ src/pages/Chat/index.tsx
@@ -1,9 +1,11 @@
 import './styles.scss';
 
-import { Brightness4, Cached, FormatSize } from '@mui/icons-material';
-import { IconButton } from '@mui/material';
+import { Brightness4, Cached, FormatSize, Send } from '@mui/icons-material';
+import { Icon, IconButton } from '@mui/material';
 import Header from 'components/Header';
 import React from 'react';
+
+import AssistentImage from '../../assets/assistente.png';
 
 export default function Chat() {
   return (
@@ -26,10 +28,41 @@ export default function Chat() {
             <h2 className="mb-0 text-dark">Cursos {'>'} Graduação</h2>
           </div>
 
-          <div className="chat-content flex-fill w-100 mw-100 p-3">
-            <div className="chat-message">
-              <div className="chat-user"></div>
-              <div className="chat-ballon"></div>
+          <div className="chat-content d-flex flex-column flex-fill w-100 mw-100 p-3">
+            <div className="chat-message d-flex left w-100">
+              <div className="chat-user rounded-circle align-self-end d-flex align-items-center justify-content-center">
+                <img
+                  src={AssistentImage}
+                  className="w-75 h-auto"
+                  alt="assistente chatbot"
+                />
+              </div>
+              <div className="chat-ballon text-white p-3">
+                <h3 className="mb-0">Qual tipo de informação você procura?</h3>
+
+                <div className="chat-menu ">
+                  <div className="chat-menu-option d-inline-block rounded-pill bg-info text-dark py-2 px-3 mt-3">
+                    <span>Insitutinal</span>
+                  </div>
+
+                  <div className="chat-menu-option d-inline-block rounded-pill bg-info text-dark py-2 px-3 mt-3">
+                    <span>Pessoas</span>
+                  </div>
+
+                  <div className="chat-menu-option selected d-inline-block rounded-pill bg-info text-dark py-2 px-3 mt-3">
+                    <span>Cursos</span>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="chat-message d-flex right w-100">
+              <div className="chat-user rounded-circle align-self-end d-flex align-items-center justify-content-center">
+                <Icon>person</Icon>
+              </div>
+              <div className="chat-ballon text-dark p-3">
+                <h3 className="mb-0">Cursos</h3>
+              </div>
             </div>
           </div>
 
@@ -40,6 +73,11 @@ export default function Chat() {
                 placeholder="Digite algo"
                 type="text"
               />
+              <div className="position-absolute">
+                <IconButton aria-label="cached">
+                  <Send />
+                </IconButton>
+              </div>
             </div>
           </div>
         </main>
