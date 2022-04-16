75513a225630df69b846d0f37ba5c7412c3c694e
danielevalverde <danierzevalverde@gmail.com>
2022-04-16 12:58:53 -0300

    add cypress

---
+++ cypress/integration/app.spec.js
@@ -0,0 +1,9 @@
+describe("APP", () => {
+  it("should logout", () => {
+    cy.visit("http://localhost:3000")
+    cy.get("#logout-button").click()
+    cy.get("#email").should('have.length', 1)
+
+    
+  })
+})
