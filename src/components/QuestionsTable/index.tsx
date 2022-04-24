08057da57f1c6b91fc86d9a58712aaeb5396d939
Marcos Adriano <marcosaps@ufba.br>
2022-04-24 20:24:29 -0300

    feat: change actions text to icons in question table

--- src/components/QuestionsTable/index.tsx
+++ src/components/QuestionsTable/index.tsx
@@ -1,5 +1,6 @@
-import { Button, CircularProgress } from '@mui/material';
-import { DataGrid, GridColDef, GridValueGetterParams } from '@mui/x-data-grid';
+import { Delete, Edit } from '@mui/icons-material';
+import { IconButton } from '@mui/material';
+import { DataGrid, GridColDef } from '@mui/x-data-grid';
 import TextModal from 'components/TextModal';
 import { useAuthContext } from 'contexts/AuthContext/hook';
 import { Question } from 'pages/Questions/types';
@@ -69,7 +70,6 @@ export default function QuestionsTable({
     {
       field: 'id',
       headerName: 'Ações',
-      minWidth: 200,
       renderCell: (item) => (
         <>
           <TextModal
@@ -79,27 +79,24 @@ export default function QuestionsTable({
             handleClose={handleCloseModal}
             handleConfirm={() => handleConfirm(openedModal as number)}
           />
-          <Button
-            variant="contained"
-            color="secondary"
-            disableElevation
-            onClick={() => handleEdit(item.row as Question)}
+          <IconButton
+            aria-label="Editar"
             data-testid="edit-button"
+            component="span"
+            onClick={() => handleEdit(item.row as Question)}
           >
-            EDITAR
-          </Button>
-
-          <Button
-            variant="contained"
-            color="warning"
-            className="ml-2"
+            <Edit />
+          </IconButton>
+          <IconButton
+            data-testid="delete-button"
+            aria-label="Deletar"
+            component="span"
             onClick={() => {
               handleOpenModal(item.row.id);
             }}
-            data-testid="delete-button"
           >
-            EXCLUIR
-          </Button>
+            <Delete />
+          </IconButton>
         </>
       ),
     },
